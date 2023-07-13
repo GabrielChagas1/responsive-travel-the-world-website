a92b431803fbe624cd783dd2265fd3fac29163a5
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-13 18:00:39 -0300

    scanning all menu items and adding show-menu class on clicked item

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,5 @@ function linkAction(){
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
