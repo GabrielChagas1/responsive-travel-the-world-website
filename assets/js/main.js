e22a7a29cec59524632bc478ab19f5eb6abf9636
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-30 22:59:03 -0300

    creating script show scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,12 @@ function blurHeader(){
     if(this.scrollY >= 50) header.classList.add('blur-header'); else header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
