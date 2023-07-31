3701a11f6dd3a331eaf487d60d604c1fd0a08f7e
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-30 22:59:50 -0300

    creating animation site

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,3 +73,9 @@ const sr = ScrollReveal({
     delay: 400,
     reset: true //Animation repeat
 })
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
+sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
+sr.reveal(`.about__data, .join__image`, {origin: 'rigth'})
+sr.reveal(`.about__image, .join__data`, {origin: 'left'})
+sr.reveal(`.popular__card`, {interval: 200})
